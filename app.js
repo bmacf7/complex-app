e169b14e2a00d7b55ec2daadaa10f3443c97d91d
Byron Machin <bmachin7@gmail.com>
2022-09-02 21:42:18 -0500

    Envío de datos habilitado.

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 import router from "./router.js";
 
+// Enabling accepting data in the common ways.
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use("/public", express.static("public"));
 app.set("views", "views");
 app.set("view engine", "ejs");

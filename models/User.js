310a7afd738c39cbd25157df74129b0b456231c4
Byron Machin <bmachin7@gmail.com>
2022-09-09 19:06:30 -0500

    Se tratan los datos y se agregan a la BD.

--- models/User.js
+++ models/User.js
@@ -1,8 +1,28 @@
 import validator from "validator";
+import { client } from "../app.js";
 
 export let User = function (data) {
   this.data = data;
   this.errors = [];
+};
+
+User.prototype.cleanUp = function () {
+  if (typeof this.data.username != "string") {
+    this.data.username = "";
+  }
+  if (typeof this.data.email != "string") {
+    this.data.email = "";
+  }
+  if (typeof this.data.password != "string") {
+    this.data.password = "";
+  }
+
+  // get rid of any bogus properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password,
+  };
 };
 
 User.prototype.validate = function () {
@@ -38,7 +58,13 @@ User.prototype.validate = function () {
 };
 
 User.prototype.register = function () {
+  // Step 1.- Validate incoming data
+  this.cleanUp();
   this.validate();
+
   // Step 2.- only if there are no validation errors
   // then save the user data into a database
+  if (!this.errors.length) {
+    client.db().collection("users").insertOne(this.data);
+  }
 };
